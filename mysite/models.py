859a6d069443f262bb6111526821dd199c6f2244
Ianssss-dd <wsadwsad1937@gmail.com>
2024-02-27 01:40:13 +0800

    add author column

--- mysite/models.py
+++ mysite/models.py
@@ -5,6 +5,7 @@ class Post(models.Model):
     title = models.CharField(max_length = 200)
     slug = models.CharField(max_length = 200)
     body = models.TextField()
+    author = models.CharField(max_length = 200, default="Rogue")
     pub_date = models.DateTimeField(auto_now_add=True)
     
     class Meta:
